e60c77c3c9623c54860f861efc4555e252cfaa6e
Marshall Abrams <marshall@logical.net>
2021-11-23 11:35:57 -0600

    Initial commit

--- src/clj/corpsfini/display.clj
+++ src/clj/corpsfini/display.clj
@@ -1,16 +1,18 @@
-(ns corpsfini.display'
+(ns corpsfini.display
     (:require [nextjournal.clerk :as clerk]))
 
 (def powers-of-x 
-  "A lazy infinite sequence of strings of the form
-  (1, \"x\", \"x^2\, ... \"x^n\", ...)."
-  (concat ["1" "x"] 
-          (map (fn [e] (str "x^" e)) (rest (range)))))
+  "A lazy infinite sequence of strings of the form:
+  (\"x\", \"x^2\" ... \"x^n\", ...).
+  Note that the first, constant term is not included."
+  (cons "x" (map (fn [e] (str "x^" e)) (rest (range)))))
 
+;; FIXME off-by-one errors
+;; ALSO need to special-case when coef is 1
 (defn texify-terms
   [poly]
   (let [integer-coef (first poly)
-        x-coefs (rest-poly)]
+        x-coefs (rest poly)]
     (keep identity
           (cons
             (if (zero? integer-coef)  ; constant term is special case
@@ -26,10 +28,11 @@
 
 (defn texify-poly
   [poly]
-  (join
+  ;; FIXME
+  )
 
 (defn clerk-poly
   [poly]
-  (clerk/tex (texify-polynomial poly)))
+  (clerk/tex (texify-poly poly)))
 
 
