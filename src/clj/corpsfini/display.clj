35febbcf59c1a9f76183f64a0d62e4fb18caa297
Marshall Abrams <marshall@logical.net>
2021-11-23 20:55:23 -0600

    working version of texify-terms

--- src/clj/corpsfini/display.clj
+++ src/clj/corpsfini/display.clj
@@ -5,22 +5,20 @@
   "A lazy infinite sequence of strings of the form:
   (\"x\", \"x^2\" ... \"x^n\", ...).
   Note that the first, constant term is not included."
-  (cons "x" (map (fn [e] (str "x^" e)) (rest (range)))))
+  (cons "x" (map (fn [e] (str "x^{" e "}")) (drop 2 (range))))) ; first expt is 2
 
-;; FIXME off-by-one errors
-;; ALSO need to special-case when coef is 1
 (defn texify-terms
   [poly]
   (let [integer-coef (first poly)
         x-coefs (rest poly)]
-    (keep identity
+    (keep identity  ; squash out the nils
           (cons
             (if (zero? integer-coef)  ; constant term is special case
               nil
               integer-coef)
-            (map (fn [coef power] (if (zero? coef)
-                                    nil
-                                    (str coef power)))
+            (map (fn [coef power] (cond (zero? coef) nil
+                                        (= 1 coef) power
+                                        :else (str coef power)))
                  x-coefs
                  powers-of-x)))))
 
