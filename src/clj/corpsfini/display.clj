13a070f2080a0a8ce0177b788c2eb39376d3bceb
Marshall Abrams <marshall@logical.net>
2021-11-23 21:14:17 -0600

    revised texify-terms, defined texify-poly

--- src/clj/corpsfini/display.clj
+++ src/clj/corpsfini/display.clj
@@ -1,33 +1,29 @@
 (ns corpsfini.display
-    (:require [nextjournal.clerk :as clerk]))
+    (:require [nextjournal.clerk :as clerk]
+              [clojure.string :as string]))
 
 (def powers-of-x 
   "A lazy infinite sequence of strings of the form:
   (\"x\", \"x^2\" ... \"x^n\", ...).
   Note that the first, constant term is not included."
-  (cons "x" (map (fn [e] (str "x^{" e "}")) (drop 2 (range))))) ; first expt is 2
+  (concat [1 "x"] (map (fn [e] (str "x^{" e "}")) (drop 2 (range))))) ; first expt is 2
 
 (defn texify-terms
   [poly]
-  (let [integer-coef (first poly)
-        x-coefs (rest poly)]
-    (keep identity  ; squash out the nils
-          (cons
-            (if (zero? integer-coef)  ; constant term is special case
-              nil
-              integer-coef)
-            (map (fn [coef power] (cond (zero? coef) nil
-                                        (= 1 coef) power
-                                        :else (str coef power)))
-                 x-coefs
-                 powers-of-x)))))
-
+  (map (fn [coef power] (cond (zero? coef) nil
+                              (= 1 power) coef
+                              (= 1 coef) power
+                              :else (str coef power)))
+       poly
+       powers-of-x))
 
 
 (defn texify-poly
   [poly]
-  ;; FIXME
-  )
+  (string/join " + "
+    (keep identity  ; squashout the nils
+          (texify-terms poly))))
+  
 
 (defn clerk-poly
   [poly]
